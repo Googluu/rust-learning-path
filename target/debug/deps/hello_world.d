77bed1ca7aece4da60dc804a7ba9c2c5f8abd8a8
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-04-28 21:28:32 -0500

    Firts hello world con rust

---
+++ target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\sn3yd\rust-learning-path\hello-world\hello-world\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Users\sn3yd\rust-learning-path\hello-world\hello-world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/hello-world.d
@@ -0,0 +1,1 @@
+C:\Users\sn3yd\rust-learning-path\hello-world\hello-world\target\debug\hello-world.exe: C:\Users\sn3yd\rust-learning-path\hello-world\hello-world\src\main.rs
