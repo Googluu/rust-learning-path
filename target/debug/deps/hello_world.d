64482f62c96cf44954543a7f821a25c09617fcf7
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-04-28 21:31:42 -0500

    target-coff-001

--- target/debug/deps/hello_world.d
+++
@@ -1,5 +0,0 @@
-C:\Users\sn3yd\rust-learning-path\hello-world\hello-world\target\debug\deps\hello_world.exe: src\main.rs
-
-C:\Users\sn3yd\rust-learning-path\hello-world\hello-world\target\debug\deps\hello_world.d: src\main.rs
-
-src\main.rs:
--- target/debug/hello-world.d
+++
@@ -1,1 +0,0 @@
-C:\Users\sn3yd\rust-learning-path\hello-world\hello-world\target\debug\hello-world.exe: C:\Users\sn3yd\rust-learning-path\hello-world\hello-world\src\main.rs
